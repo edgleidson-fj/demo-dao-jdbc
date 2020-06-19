f6f3102a0c5fca596f1299615d88f1efe9c3799f
Edgleidson <wake-up88@hotmail.com>
2020-06-19 19:46:19 -0300

    Entidade Departamento.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
-//Aula 237 - DAO JDBC - (Criar Projeto).
+
+import model.entidade.Departamento;
+
+//Aula 238 - DAO JDBC - (Classe Departamento).
 public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		Departamento obj = new Departamento(1, "BOOKS");
+		System.out.println(obj);
 	}
 
 }
