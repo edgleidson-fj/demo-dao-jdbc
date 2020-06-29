995e67a50061e9370cfe84f1ea98ca4c3dbd18d1
Edgleidson <wake-up88@hotmail.com>
2020-06-29 18:44:08 -0300

    Implementando INSERT [Vendedor].

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -7,7 +8,7 @@ import model.dao.VendedorDao;
 import model.entidade.Departamento;
 import model.entidade.Vendedor;
 
-//Aula 246 - DAO JDBC - (Implementando buscarTudo - Vendedor).
+//Aula 247 - DAO JDBC - (Implementando INSERT - Vendedor).
 public class Main {
 
 	public static void main(String[] args) {
@@ -20,18 +21,22 @@ public class Main {
 		
 		System.out.println("\n=== TESTE 2: seller buscarPorDepartamento  ===");	
 		Departamento dep = new Departamento(2, null);
-		List<Vendedor> lista = vendDao.buscarPorDepartamento(dep);
-		
+		List<Vendedor> lista = vendDao.buscarPorDepartamento(dep);		
 		for(Vendedor v : lista) {
 			System.out.println(v);
 		}
 		
 		System.out.println("\n=== TESTE 3: seller buscarTudo  ===");	
-		lista = vendDao.buscarTudo();
-		
+		lista = vendDao.buscarTudo();		
 		for(Vendedor v : lista) {
 			System.out.println(v);
 		}
+		
+		System.out.println("\n=== TESTE 4: seller Insert  ===");
+		Vendedor novoVendedor = new Vendedor(
+				null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep); //Reutilizando a variavel (dep) do teste 2.
+		vendDao.inserir(novoVendedor);
+		System.out.println("INSERT realizado com sucesso! Novo ID: "+ novoVendedor.getId());     
 	}
 
 }
--- src/model/dao/implementacao/VendedorDaoJDBC.java
+++ src/model/dao/implementacao/VendedorDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,6 +27,38 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public void inserir(Vendedor obj) {
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) " 
+					+ "VALUES  (?, ?, ?, ?, ?) ",
+					Statement.RETURN_GENERATED_KEYS); // Retornar o ID do Vendedor após o INSERT.
+
+			ps.setString(1, obj.getNome());
+			ps.setString(2, obj.getEmail());
+			ps.setDate(3, new java.sql.Date(obj.getNascimento().getTime())); // Date.
+			ps.setDouble(4, obj.getSalarioBase());
+			ps.setInt(5, obj.getDepartamento().getId()); // Departamento.
+
+			int linhasAfetadas = ps.executeUpdate();
+			if (linhasAfetadas > 0) {
+				ResultSet rs = ps.getGeneratedKeys(); // Pegando o ID do Vendedor.
+				if (rs.next()) {
+					int id = rs.getInt(1); // 1ª (?).
+					obj.setId(id);
+				}
+				BD.fecharResultSet(rs);
+			} else {
+				throw new BDException("Erro no INSERT, nenhuma linha foi afetada!");
+			}
+		} 
+		catch (SQLException ex) {
+			throw new BDException(ex.getMessage());
+		} 
+		finally {
+			BD.fecharStatement(ps);
+		}
 	}
 
 	@Override
@@ -42,62 +75,56 @@ public class VendedorDaoJDBC implements VendedorDao {
 		ResultSet rs = null;
 		try {
 			ps = connection.prepareStatement(
-					"SELECT seller.*, department.Name as DepName " 
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id " 
-					+ "WHERE seller.Id = ? ");
+					"SELECT seller.*, department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ? ");
 			ps.setInt(1, id);
 			rs = ps.executeQuery();
-			
+
 			if (rs.next()) {
 				Departamento dep = instanciarFuncaoDepartamento(rs);
 				Vendedor vend = instaciarFuncaoVendedor(rs, dep);
 				return vend;
 			}
 			return null;
-		} 
-		catch (SQLException ex) {
+		} catch (SQLException ex) {
 			throw new BDException(ex.getMessage());
-		} 
-		finally {
+		} finally {
 			BD.fecharStatement(ps);
 			BD.fecharResultSet(rs);
 		}
 	}
-	
+
 	@Override
 	public List<Vendedor> buscarTudo() {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
-			ps = connection.prepareStatement(
-					"SELECT seller.*,department.Name as DepName  FROM seller "
-					+ "INNER JOIN department  ON seller.DepartmentId = department.Id "
-					+ "ORDER BY Name ");
+			ps = connection.prepareStatement("SELECT seller.*,department.Name as DepName  FROM seller "
+					+ "INNER JOIN department  ON seller.DepartmentId = department.Id " + "ORDER BY Name ");
 			rs = ps.executeQuery();
-			
+
 			List<Vendedor> lista = new ArrayList<>();
-			
-			//Map<chave , valor> - Para controlar a não repetição do Departamento desnecessáriamente.
+
+			// Map<chave , valor> - Para controlar a não repetição do Departamento
+			// desnecessáriamente.
 			Map<Integer, Departamento> map = new HashMap<>();
-			
-			while (rs.next()) {				
+
+			while (rs.next()) {
 				Departamento depNaoRepetido = map.get(rs.getInt("DepartmentId"));
-				
-				//Se não existir Map com ID do Departamento o sistema cria um, se não, utiliza o que já existe no Map. 
-				if(depNaoRepetido == null) {
-					depNaoRepetido = instanciarFuncaoDepartamento(rs);	
-					map.put(rs.getInt("DepartmentId"), depNaoRepetido); //Inserir no Map<chave , valor>.
-				}				
+
+				// Se não existir Map com ID do Departamento o sistema cria um, se não, utiliza
+				// o que já existe no Map.
+				if (depNaoRepetido == null) {
+					depNaoRepetido = instanciarFuncaoDepartamento(rs);
+					map.put(rs.getInt("DepartmentId"), depNaoRepetido); // Inserir no Map<chave , valor>.
+				}
 				Vendedor vend = instaciarFuncaoVendedor(rs, depNaoRepetido);
-				lista.add(vend); 
+				lista.add(vend);
 			}
 			return lista;
-		} 
-		catch (SQLException ex) {
+		} catch (SQLException ex) {
 			throw new BDException(ex.getMessage());
-		} 
-		finally {
+		} finally {
 			BD.fecharStatement(ps);
 			BD.fecharResultSet(rs);
 		}
@@ -108,49 +135,47 @@ public class VendedorDaoJDBC implements VendedorDao {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
-			ps = connection.prepareStatement(
-					"SELECT seller.*,department.Name as DepName  FROM seller "
-					+ "INNER JOIN department  ON seller.DepartmentId = department.Id "
-					+ "WHERE DepartmentId = ? "
+			ps = connection.prepareStatement("SELECT seller.*,department.Name as DepName  FROM seller "
+					+ "INNER JOIN department  ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? "
 					+ "ORDER BY Name ");
 			ps.setInt(1, departamento.getId());
 			rs = ps.executeQuery();
-			
+
 			List<Vendedor> lista = new ArrayList<>();
-			
-			//Map<chave , valor> - Para controlar a não repetição do Departamento desnecessáriamente.
+
+			// Map<chave , valor> - Para controlar a não repetição do Departamento
+			// desnecessáriamente.
 			Map<Integer, Departamento> map = new HashMap<>();
-			
-			while (rs.next()) {				
+
+			while (rs.next()) {
 				Departamento depNaoRepetido = map.get(rs.getInt("DepartmentId"));
-				
-				//Se não existir Map com ID do Departamento o sistema cria um, se não, utiliza o que já existe no Map. 
-				if(depNaoRepetido == null) {
-					depNaoRepetido = instanciarFuncaoDepartamento(rs);	
-					map.put(rs.getInt("DepartmentId"), depNaoRepetido); //Inserir no Map<chave , valor>.
-				}				
+
+				// Se não existir Map com ID do Departamento o sistema cria um, se não, utiliza
+				// o que já existe no Map.
+				if (depNaoRepetido == null) {
+					depNaoRepetido = instanciarFuncaoDepartamento(rs);
+					map.put(rs.getInt("DepartmentId"), depNaoRepetido); // Inserir no Map<chave , valor>.
+				}
 				Vendedor vend = instaciarFuncaoVendedor(rs, depNaoRepetido);
-				lista.add(vend); 
+				lista.add(vend);
 			}
 			return lista;
-		} 
-		catch (SQLException ex) {
+		} catch (SQLException ex) {
 			throw new BDException(ex.getMessage());
-		} 
-		finally {
+		} finally {
 			BD.fecharStatement(ps);
 			BD.fecharResultSet(rs);
 		}
 	}
-	
+
 	// Função Departamento(ResultSet) - Propagando Exception.
-		private Departamento instanciarFuncaoDepartamento(ResultSet rs) throws SQLException {
-			Departamento dep = new Departamento();
-			dep.setId(rs.getInt("DepartmentId"));
-			dep.setNome(rs.getString("DepName"));
-			return dep;
-		}
-	
+	private Departamento instanciarFuncaoDepartamento(ResultSet rs) throws SQLException {
+		Departamento dep = new Departamento();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setNome(rs.getString("DepName"));
+		return dep;
+	}
+
 	// Função Vendedor(ResultSet, Departamento).
 	private Vendedor instaciarFuncaoVendedor(ResultSet rs, Departamento dep) throws SQLException {
 		Vendedor vend = new Vendedor();
